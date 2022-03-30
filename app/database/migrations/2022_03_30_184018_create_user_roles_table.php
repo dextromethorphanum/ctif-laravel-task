840ece653f36425710b45a17f19090d2fcfebd43
Vitaliy Reusch <crossfield.u@gmail.com>
2022-03-30 22:20:30 +0300

    Add migrations for `roles` and `user_roles`.

---
+++ app/database/migrations/2022_03_30_184018_create_user_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->integer('role_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('role_id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        # TODO: drop foreign key.
+        Schema::dropIfExists('user_roles');
+    }
+}
