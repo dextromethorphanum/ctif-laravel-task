a52d18c14e9fda10d9b5d8a9e806b27a16a7f393
Vitaliy Reusch <crossfield.u@gmail.com>
2022-03-30 22:19:07 +0300

    Ability to seed tables with a single command: `artisan db:seed`.

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            EcoCodeSeeder::class,
+            DistrictSeeder::class,
+            LocalitySeeder::class
+        ], true);
     }
 }
