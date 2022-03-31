499d24c5c700f80cda88631e5882f19d3e64bb35
Vitaliy Reusch <crossfield.u@gmail.com>
2022-03-31 17:34:04 +0300

    Integration of Blade.

--- app/resources/views/ctif.blade.php
+++ app/resources/views/ctif.blade.php
@@ -1,79 +1,8 @@
-<!DOCTYPE html>
-<head>
-	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/select2.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/reset-style.css') }}">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-	<meta charset="UTF-8">
-	<title>{{ env('APP_NAME') }}</title>
-</head>
-<body>
-	<div class="logo">
-		<div id="wrapper">
-			<div class="logo__inner">
-				<div class="logo__main">
-					<img src="{{ asset('img/logo.png') }}" alt="Guvernul Republicii Moldova" id="logo">
-					<h2 id="site-name">
-						Ministerul Finanțelor
-					</h2>
-				</div>
-				<div class="logo__second">
-					<div class="logo__list">
-						<a href="#" class="logo__link">Acasă</a>
-						<a href="#" class="logo__link">Programul ”Prima Casă”</a>
-						<a href="#" class="logo__link">Contacte</a>
-					</div>
-					<div class="logo__country">
-						<img class="logo__country-item" src="{{ asset('img/flags/md.png') }}" alt="">
-						<img class="logo__country-item" src="{{ asset('img/flags/ru.png') }}" alt="">
-						<img class="logo__country-item" src="{{ asset('img/flags/us.png') }}" alt="">
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+@extends('layouts.ctifbase')
+@section('page_title', 'Generarea codului IBAN pentru încasări')
 
-	<header class="header">
-		<div id="wrapper">
-			<div class="header__inner">
-				<div class="header__menu">
-					<ul>
-						<li><a href="#" class="header__link">Despre Minister</a></li>
-						<li><a href="#" class="header__link">Legislatie</a></li>
-						<li><a href="#" class="header__link">Transparenta decizionala</a></li>
-						<li><a href="#" class="header__link">Media</a></li>
-						<li><a href="#" class="header__link">Finantele publice locale</a></li>
-						<li><a href="#" class="header__link">Asistenta externa</a></li>
-						<li><a href="#" class="header__link">Cariere</a></li>
-					</ul>
-				</div>
-				<form class="header__input">
-					<input class="header__input-item" type="text">
-					<button type="submit"></button>
-				</form>
-				<div class="header__social">
-					<img src="{{ asset('img/facebook-gray.png') }}" alt="">
-					<img src="{{ asset('img/youtube-gray.png') }}" alt="">
-					<img src="{{ asset('img/rss-gray.png') }}" alt="">
-				</div>
-			</div>
-		</div>
-	</header>
-
-	<div class="main">
-		<div id="wrapper">
-			<div class="main__content">
-				<div class="main__nav">
-					<a href="https://mf.gov.md/ro" class="main__nav-link">Acasa</a><p class="main__symbol">»</p><p class="main__text"> Generarea codului IBAN pentru încasări</p>
-				</div>
-
-			</div>
-		</div>
-
-	</div>
-
+@section('content')
+	@parent
 	<div class="rtecenter">
 		<form class="iban_form" action="/iban" method="post" name="iban_form">
 			<ul>
@@ -131,20 +60,4 @@
 		</form>
 		<div id="iban-placeholder"> </div>
 	</div>
-
-	<footer class="footer">
-		<div id="wrapper">
-
-		</div>
-	</footer>
-
-	<script>
-		var kdEco = '' ;
-		var kdRaion = '' ;
-		var kdLoc = '' ;
-	</script>
-	<script type="text/javascript" src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/select2.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
-</body>
-</html>
+@endsection
