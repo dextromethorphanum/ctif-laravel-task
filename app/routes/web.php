12515baf8c068c32adb4a573ddc52142f4b386a5
Vitaliy Reusch <crossfield.u@gmail.com>
2022-04-02 22:40:53 +0300

    Clean up unused things.

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\IBANController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
