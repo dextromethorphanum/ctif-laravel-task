de53d14d990ab49e5e1ada4fa8ca8fab96c54a7d
Vitaliy Reusch <crossfield.u@gmail.com>
2022-04-05 14:22:26 +0300

    Fix view('admin.users.create'), fix seeding.

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('admin.create-user', ['roles' => Role::all()]);
+        return view('admin.users.create', ['roles' => Role::all()]);
     }
 
     /**
--- app/database/seeders/UserSeeder.php
+++ app/database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\District;
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,11 +17,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $admin = User::create([
+        $id = DB::table('users')->insertGetId([
             'name' => 'admin',
             'email' => 'admin@localhost',
             'password' => Hash::make('admin@localhost'),
         ]);
+        $admin = User::find($id);
         $admin->roles()->sync([$admin->getRoleIdByName('admin')]);
 
         define('OPERATOR_DISTRICT_COUNT', 5);
@@ -30,15 +30,16 @@ class UserSeeder extends Seeder
 
         for($i = 0; $i != OPERATOR_DISTRICT_COUNT; $i++)
         {
-            $operator_raion = User::create([
+            $id = DB::table('users')->insertGetId([
                 'name' => 'operator_raion_' . $districts[$i]->code,
                 'email' => 'operator_raion_' . $districts[$i]->code . '@localhost',
                 'password' => Hash::make('operator_raion'),
             ]);
 
+            $operator_raion = User::find($id);
             $operator_raion->roles()->sync([$operator_raion->getRoleIdByName('operator_raion')]);
             DB::table('user_roles')
-                ->where('user_id', $operator_raion->id)
+                ->where('user_id', $id)
                 ->where('role_id', $operator_raion->getRoleIdByName('operator_raion'))
                 ->update(['additional_code' => $districts[$i]->code]);
         }
